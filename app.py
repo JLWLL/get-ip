b1eaab65d847bada910b8e6df1bcd9d5c1ea3181
JLWL <1174909880@qq.com>
2022-11-20 21:26:34 +0800

    UPDATE

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+
+from flask import Flask, render_template, redirect, url_for, request, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('getip-test.html')
+
+
+@app.route('/get_ip', methods=['GET', 'POST'])
+def get_ip():
+    if request.method == 'POST':
+        ip = request.form.get('ip')
+        print("访问者的IP为：" + str(ip))
+        return json.dumps(ip)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5501)
